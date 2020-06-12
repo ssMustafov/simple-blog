4885dc7dadd9a4a427a04b079c92ecba7d1a030c
Sinan Mustafov <ss.mustafov@gmail.com>
2020-06-12 20:27:50 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/example/blog/SimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBlogApplication.class, args);
+    }
+
+}
